fd73e106243acc451c45f862230865e152969579
kapil.madan <kapilmadan92@gmail.com>
2024-02-01 12:39:56 +0530

    multithreading in browser

--- src/Utilities/BrowserConfig.java
+++ src/Utilities/BrowserConfig.java
@@ -11,10 +11,12 @@ import Keywords.TestReport;
 
 public class BrowserConfig extends ParentUtilities {
 	
+	@SuppressWarnings("null")
 	public static Browser getBrowser(String BrowserType, ThreadLocal<ExtentTest> logger) {
 		
-		Playwright playwright = Playwright.create();
-		Browser browser = null;
+		ThreadLocal<Playwright> playwright = null;
+		playwright.set(Playwright.create());
+		ThreadLocal<Browser> browser = null;
 		LaunchOptions lp = new LaunchOptions();
 		
 		if(System.getProperty("Headless Execution").equalsIgnoreCase("Yes")) {
@@ -30,26 +32,26 @@ public class BrowserConfig extends ParentUtilities {
 		if(BrowserType.equalsIgnoreCase("Chrome")){
 			TestReport.Log(logger, Status.INFO, "Launching Chrome Browser.....");
 			lp.setChannel("chrome");
-			browser= playwright.chromium().launch(lp);
+			browser.set(playwright.get().chromium().launch(lp));;
 			TestReport.Pass(logger, Status.INFO, "Chrome Browser Launched Successfully");
 			
 		}
 		else if(BrowserType.equalsIgnoreCase("Edge")) {
 			TestReport.Log(logger, Status.INFO, "Launching Edge Browser.....");
 			lp.setChannel("msedge");
-			browser= playwright.firefox().launch(lp);
+			browser.set(playwright.get().chromium().launch(lp));
 			TestReport.Pass(logger, Status.INFO, "Edge Browser Launched Successfully");
 			
 		}
 		else if(BrowserType.equalsIgnoreCase("Firefox")) {
 			TestReport.Log(logger, Status.INFO, "Launching Firefox Browser.....");
-			browser= playwright.firefox().launch(lp);
+			browser.set(playwright.get().firefox().launch(lp));
 			TestReport.Pass(logger, Status.INFO, "Firefox Browser Launched Successfully");
 			
 		}
 		else if(BrowserType.equalsIgnoreCase("Safari")) {
 			TestReport.Log(logger, Status.INFO, "Launching Safari Browser.....");
-			browser= playwright.webkit().launch(lp);
+			browser.set(playwright.get().webkit().launch(lp));
 			TestReport.Pass(logger, Status.INFO, "Safari Browser Launched Successfully");
 			
 		}
@@ -57,7 +59,7 @@ public class BrowserConfig extends ParentUtilities {
 			TestReport.Fail(logger, Status.FAIL, "Safari Browser Launched Successfully");
 		}
 		
-		return browser;
+		return browser.get();
 	}
 
 }
